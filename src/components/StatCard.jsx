4b1f18eac88c415b991622995a42540bf4e43f6d
canhamzacode <abdulmuizzayo6@gmail.com>
2023-12-10 15:17:39 +0100

    fixed in consistency

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
 const StatCard = ({ title, value, time, bg }) => {
+  const [background, setBackground] = useState("");
+
+  useEffect(() => {
+    const getBackgroundClass = (bg) => {
+      switch (bg) {
+        case "foundation-blue-b-300":
+          return "bg-foundation-blue-b-300";
+        case "foundation-tblue-t-400":
+          return "bg-foundation-tblue-t-400";
+        case "foundation-blue-b-400":
+          return "bg-foundation-blue-b-400";
+        default:
+          return "bg-black";
+      }
+    };
+
+    setBackground(getBackgroundClass(bg));
+  }, [bg, title, value, time]);
+
   return (
-    <div className={`w-full p-4 rounded-xl bg-${bg}`}>
-      <div className={`w-full rounded-xl  bg-white p-4`}>
+    <div className={`w-full p-4 rounded-xl ${background}`}>
+      <div className="w-full rounded-xl bg-white p-4">
         <div className="flex flex-col gap-6">
           <p className="text-base">{title}</p>
           <h3 className="text-3xl">{value}</h3>
